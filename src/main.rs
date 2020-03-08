0c12e23623d6037c6c122c5e082682298188e730
coffeenotfound <code.katzer@gmail.com>
2020-03-08 22:48:36 +0100

    Add (more or less working) NvOptimusEnablement export symbol static

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,15 @@ pub mod asset;
 pub mod structured_shader_language;
 pub mod btex;
 
+/// This should force NVIDIA Optimus to switch to
+/// the discrete GPU on notebooks.
+/// But it doesn't seem to work or maybe my notebook
+/// just doesn't support Optimus.
+#[no_mangle]
+#[used]
+#[export_name = "NvOptimusEnablement"]
+pub static NvOptimusEnablement: u32 = 0x1;
+
 fn main() {
 	demo::start();
 }
