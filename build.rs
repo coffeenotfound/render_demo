7e90ecd2898f78b8b3dfc236f9ce8b2119e6b5d9
coffeenotfound <code.katzer@gmail.com>
2020-03-08 22:43:30 +0100

    Make libglfw3 rustc-link-search path an env variable in build.rs

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 
 fn main() {
 	// Add glfw to link path
-	println!(r"cargo:rustc-link-search=C:\Tools\glfw-3.3.bin.WIN64\lib-mingw-w64");
+	println!(r"cargo:rustc-link-search={}", env!("GLFW_MINGW_LIBS"));
 }
