5938a4e9118bc7a775639f5e61b8c2817019556a
Jan Katzer <code.katzer@gmail.com>
2020-05-13 23:17:21 +0200

    Add s3tc and bptc compression GL extensions to gl_bindings build script

--- gl_bindings/build.rs
+++ gl_bindings/build.rs
@@ -9,9 +9,11 @@ fn main() {
 	let mut gl_file = File::create(&Path::new(&gl_dest).join("bindings.rs")).unwrap();
 	
 	let exts: &[&str] = &[
+		"GL_KHR_debug",
 		"GL_ARB_direct_state_access",
 		"GL_ARB_clip_control",
-		"GL_KHR_debug",
+		"GL_EXT_texture_compression_s3tc",
+		"GL_ARB_texture_compression_bptc",
 	];
 	Registry::new(Api::Gl, (4, 5), Profile::Compatibility, Fallbacks::All, exts)
 		.write_bindings(GlobalGenerator, &mut gl_file)
