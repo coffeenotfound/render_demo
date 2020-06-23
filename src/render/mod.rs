46ebd3ceb74fd089c0ac2bf40fd224932a179a48
Jan Katzer <code.katzer@gmail.com>
2020-06-23 02:03:33 +0200

    Add module ehaa in ::render

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -4,6 +4,7 @@ pub mod bloom;
 pub mod clustered;
 pub mod performance;
 pub mod teapot;
+pub mod ehaa;
 
 mod render_global; pub use render_global::*;
 mod graphics_settings; pub use graphics_settings::*;
